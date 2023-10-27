7976ac1b738e8d3896924ed4c0d37968b2f00df7
Fredrik Hoelseter <fredhoe@stud.ntnu.no>
2023-10-27 14:07:58 +0200

    Add employee

--- src/main/java/com/example/heroku/deployment/controllers/EmployeeController.java
+++ src/main/java/com/example/heroku/deployment/controllers/EmployeeController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -18,5 +21,12 @@ public class EmployeeController {
     @GetMapping("/get")
     public ResponseEntity<List<Employee>> getEmployees() {
         return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @PostMapping("/save/{id}/{name}")
+    public void saveEmployee(@PathVariable("id") Long id, @PathVariable("name") String name) {
+        Employee employee = new Employee(id, name);
+        employeeService.save(employee);
+
     }
 }
